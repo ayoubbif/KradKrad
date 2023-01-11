7d72e480d4548d5aaddce9e7c4de5fb394a6e360
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-11 08:43:20 +0100

    Added window manager

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import type { ReactElement } from 'react';
-import ProcessLoader from '../contexts/process';
+import type { ReactElement } from "react";
+import { ProcessProvider } from "../contexts/process";
+import WindowManager from "../components/system/WindowManager";
 
 export default function Home(): ReactElement {
   return (
-    <ProcessLoader/>
+    <ProcessProvider>
+      <WindowManager />
+    </ProcessProvider>
   );
 }
 
