6a654eb62912efd3d96fdca81e3fa93290fb3fe3
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-11 22:09:50 +0100

    Refactored tests

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -1,13 +1,13 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import StyledApp from '../../components/pages/StyledApp';
 import Index from '../../pages/index';
 
-test('renders index page', () => {
-  const { getByRole } = render(
+test('renders main role', () => {
+  render(
     <StyledApp>
       <Index />
     </StyledApp>
   );
 
-  expect(getByRole('main')).toBeInTheDocument();
+  expect(screen.getByRole('main')).toBeInTheDocument();
 });
