162de1845e58685977e8b4a71bf37e138fd3f85a
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-12 21:02:15 +0100

    The taskbar is no longer dynamic

--- hooks/useProcessContextState.ts
+++ hooks/useProcessContextState.ts
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import { Taskbar } from '../utils/processDirectory';
 import type { ProcessContextState, Processes } from '../types/contexts/process';
 
 const useProcessContextState = (): ProcessContextState => {
-  const [processes] = useState<Processes>({ Taskbar });
+  const [processes] = useState<Processes>({});
 
   return { processes };
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import type { ReactElement } from 'react';
 import { ProcessProvider } from '../contexts/process';
 import ProcessLoader from '../components/system/Processes/ProcessLoader';
 import Desktop from '../components/system/Desktop';
+import Taskbar from '../components/system/Taskbar';
 
 const Home = (): ReactElement => (
   <Desktop>
     <ProcessProvider>
+      <Taskbar />
       <ProcessLoader />
     </ProcessProvider>
   </Desktop>
