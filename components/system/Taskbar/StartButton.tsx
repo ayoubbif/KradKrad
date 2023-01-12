fdefbe107ff57966e79f25e6d679c0c45bd0c179
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-12 01:27:57 +0100

    Added tooltip do start button with basic HTML

--- components/system/Taskbar/StartButton.tsx
+++ components/system/Taskbar/StartButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import StyledStartButton from '../../../styles/components/system/Taskbar/StyledStartButton';
 
 const StartButton = () => (
-  <StyledStartButton type="button">ICON</StyledStartButton>
+  <StyledStartButton type="button" title="Start">ICON</StyledStartButton>
 );
 
 export default StartButton;
