e1d79da6257c88513b6690c7a78356b7b568fba4
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-14 05:41:09 +0100

    Add some style to StyledFileManager

--- styles/components/system/Files/StyledFileManager.ts
+++ styles/components/system/Files/StyledFileManager.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
-const StyledFileManager = styled.ol``;
+const StyledFileManager = styled.ol`
+  column-count: 1px;
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: repreat(auto-fill, 75px);
+  grid-template-rows: repreat(auto-fill, 70px);
+  padding: 5px 0;
+  row-gap: 20px;
+  height: ${({ theme }) => `calc(100% - ${theme.sizes.taskbar.height})`};
+`;
 
 export default StyledFileManager;
