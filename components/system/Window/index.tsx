2cd9b79ca69eb6331dbb36d6c76b7848330e182f
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-14 01:51:03 +0100

    Added Rnd and its defaults

--- components/system/Window/index.tsx
+++ components/system/Window/index.tsx
@@ -2,19 +2,31 @@ import { useProcesses } from 'contexts/process';
 import StyledWindow from 'styles/components/system/Window/StyledWindow';
 import { ProcessComponentProps } from '../Processes/RenderProcess';
 import TitleBar from './TitleBar';
+import { Rnd } from 'react-rnd';
+import useResizable from 'hooks/useResizable';
+import rndDefaults from 'utils/rndDefaults';
 
 const Window: React.FC<ProcessComponentProps> = ({ children, id }) => {
   const {
     processes: {
-      [id]: { minimized }
+      [id]: { maximized, minimized }
     }
   } = useProcesses();
 
+  const { height, width, updateSize } = useResizable(maximized);
+
   return (
-    <StyledWindow minimized={minimized}>
-      <TitleBar id={id} />
-      {children}
-    </StyledWindow>
+    <Rnd
+      enableResizing={!maximized}
+      size={{ height, width }}
+      onResizeStop={updateSize}
+      {...rndDefaults}
+    >
+      <StyledWindow minimized={minimized}>
+        <TitleBar id={id} />
+        {children}
+      </StyledWindow>
+    </Rnd>
   );
 };
 
