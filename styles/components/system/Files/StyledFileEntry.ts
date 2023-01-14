3802f769eb5da06804ca3b5eb4576e098c8ac5aa
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-14 06:56:04 +0100

    Styled desktop grid

--- styles/components/system/Files/StyledFileEntry.ts
+++ styles/components/system/Files/StyledFileEntry.ts
@@ -1,5 +1,39 @@
 import styled from 'styled-components';
 
-const StyledFileEntry = styled.li``;
+const StyledFileEntry = styled.li`
+  padding: 5px;
+  display: flex;
+  justify-content: center;
+
+  &:hover {
+    background-color: hsla(10, 0%, 100%, 0.5);
+    border: 2px solid hsla(10, 0%, 100%, 0.5);
+    padding: 0;
+    position: relative;
+
+    &::before {
+      border: 1px solid hsla(10, 0%, 33%, 0.7);
+      content: '';
+      left: -5px;
+      position: absolute;
+      right: -5px;
+      top: -5px;
+    }
+  }
+
+  button {
+    figcaption {
+      color: #000;
+      font-size: 14px;
+      text-shadow: 0 0 1px rgba(99, 88, 77, 0.75), 0 0 2px rgba(66, 55, 44, 0.5),
+        0 0 3px rgba(33, 22, 11, 0.25), 0 0 4px rgba(00, 255, 25, 0.1);
+    }
+
+    img {
+      height: 55px;
+      width: 55px;
+    }
+  }
+`;
 
 export default StyledFileEntry;
--- styles/components/system/Files/StyledFileManager.ts
+++ styles/components/system/Files/StyledFileManager.ts
@@ -4,10 +4,8 @@ const StyledFileManager = styled.ol`
   column-count: 1px;
   display: grid;
   grid-auto-flow: column;
-  grid-template-columns: repreat(auto-fill, 75px);
-  grid-template-rows: repreat(auto-fill, 70px);
-  padding: 5px 0;
-  row-gap: 20px;
+  grid-template-columns: repeat(auto-fill, minmax(80px, 1fr));
+  grid-template-rows: repeat(auto-fill, minmax(80px, 1fr));
   height: ${({ theme }) => `calc(100% - ${theme.sizes.taskbar.height})`};
 `;
 
