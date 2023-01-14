54838b63d91f4ddec8621cc1897c5a6c8940f354
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-14 06:55:48 +0100

    Styled window

--- styles/components/system/Window/StyledWindow.ts
+++ styles/components/system/Window/StyledWindow.ts
@@ -6,8 +6,12 @@ type StyledWindowProps = {
 
 const StyledWindow = styled.section<StyledWindowProps>`
   background-color: ${({ theme }) => theme.colors.window};
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.5);
   display: ${({ minimized = false }) => (minimized ? 'none' : 'block')};
+  outline: 2px solid;
   position: absolute;
+  width: 100%;
+  height: 100%;
 `;
 
 export default StyledWindow;
