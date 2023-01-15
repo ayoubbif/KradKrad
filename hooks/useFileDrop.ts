d5b00db549943c6c378e4123d46f9589fd315a57
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-15 06:54:04 +0100

    Added file drop feature

---
+++ hooks/useFileDrop.ts
@@ -0,0 +1,37 @@
+import { useFileSystem } from 'contexts/fileSystem';
+import React, { useCallback } from 'react';
+
+const haltDragEvent = (event: React.DragEvent): void => {
+  event.preventDefault();
+  event.stopPropagation();
+};
+
+type FileDrop = {
+  onDrop: (event: React.DragEvent<HTMLElement>) => void;
+  onDragOver: (event: React.DragEvent<HTMLElement>) => void;
+};
+
+const useFileDrop = (directory: string, getFiles: () => void): FileDrop => {
+  const { fs } = useFileSystem();
+  const onDrop = useCallback(
+    (event: React.DragEvent<HTMLElement>) => {
+      haltDragEvent(event);
+
+      const { files: [file] = [] } = event.dataTransfer || {};
+      const reader = new FileReader();
+
+      reader.onload = ({ target }) =>
+        fs?.writeFile(`${directory}/${file.name}`, target?.result, getFiles);
+
+      reader.readAsArrayBuffer(file);
+    },
+    [directory, fs, getFiles]
+  );
+
+  return {
+    onDragOver: haltDragEvent,
+    onDrop
+  };
+};
+
+export default useFileDrop;
