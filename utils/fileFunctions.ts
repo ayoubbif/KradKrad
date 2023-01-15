98d2a7239392441b02ef0c5a256cce8d0cfcc81b
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-15 05:43:26 +0100

    Updated the file system

--- utils/fileFunctions.ts
+++ utils/fileFunctions.ts
@@ -7,13 +7,22 @@ type Shortcut = {
 };
 
 export const getShortcut = (path: string, fs: FSModule): Promise<Shortcut> =>
-  new Promise((resolve) => {
-    fs.readFile(path, (_error, contents = Buffer.from('')) => {
-      const { InternetShortcut = { URL: '', IconFile: '' } } = ini.parse(
-        contents.toString()
-      );
-      resolve(InternetShortcut as Shortcut);
+  new Promise((resolve, reject) => {
+    fs.readFile(path, (error, contents = Buffer.from('')) => {
+      if (error) {
+        reject(error);
+      } else {
+        const { InternetShortcut = { URL: '', IconFile: '' } } = ini.parse(
+          contents.toString()
+        );
+        resolve(InternetShortcut as Shortcut);
+      }
     });
   });
-
+export const getIconByFileExtension = (extension: string): string => {
+  switch (extension) {
+    default:
+      return '/icons/unknown.png';
+  }
+};
 export const getProcessByFileExtension = (_extension: string): string => '';
