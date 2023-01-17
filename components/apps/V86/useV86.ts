f02409bd30cb407861ab95da373ca8735d0b3ba6
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-17 03:10:46 +0100

    Clean up buffer URLs

--- components/apps/V86/useV86.ts
+++ components/apps/V86/useV86.ts
@@ -13,7 +13,7 @@ import { useFileSystem } from 'contexts/fileSystem';
 
 import { extname } from 'path';
 import { useCallback, useEffect, useState } from 'react';
-import { bufferToUrl, loadFiles } from 'utils/functions';
+import { bufferToUrl, cleanUpBufferUrl, loadFiles } from 'utils/functions';
 
 const useV86 = (
   url: string,
@@ -24,28 +24,31 @@ const useV86 = (
   const { fs } = useFileSystem();
 
   useEffect(() => {
-    if (!emulator) {
+    if (!emulator && fs && url && screenContainer?.current) {
       fs?.readFile(url, (_error, contents = Buffer.from('')) => {
         loadFiles(['/libs/v86/libv86.js']).then(() => {
           const isISO = extname(url).toLowerCase() === '.iso';
           const { deviceMemory = 8 } = navigator as NavigatorWithMemory;
           const memoryRatio = deviceMemory / 8;
 
-          setEmulator(
-            new (window as WindowWithV86Starter).V86Starter({
-              memory_size: memoryRatio * 1024 * 1024 * 1024,
-              vga_memory_size: memoryRatio * 32 * 1024 * 1024,
-              boot_order: isISO ? BOOT_CD_FD_HD : BOOT_FD_CD_HD,
-              [isISO ? 'cdrom' : 'fda']: {
-                async: false,
-                size: contents.length,
-                use_parts: false,
-                url: bufferToUrl(contents)
-              },
-              screen_container: screenContainer.current,
-              ...v86Config
-            })
+          const bufferUrl = bufferToUrl(contents);
+          const v86 = new (window as WindowWithV86Starter).V86Starter({
+            memory_size: memoryRatio * 1024 * 1024 * 1024,
+            vga_memory_size: memoryRatio * 32 * 1024 * 1024,
+            boot_order: isISO ? BOOT_CD_FD_HD : BOOT_FD_CD_HD,
+            [isISO ? 'cdrom' : 'fda']: {
+              async: false,
+              size: contents.length,
+              url: bufferUrl,
+              use_parts: false
+            },
+            screen_container: screenContainer.current,
+            ...v86Config
+          });
+          v86.add_listener('emulator-loaded', () =>
+            cleanUpBufferUrl(bufferUrl)
           );
+          setEmulator(v86);
         });
       });
     }
--- styles/generic/CustomImage.ts
+++ styles/generic/CustomImage.ts
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
+import { cleanUpBufferUrl } from 'utils/functions';
+
+const onLoad: React.ReactEventHandler<HTMLImageElement> = ({ target }) => {
+  const img = target as HTMLImageElement;
+
+  if (img.src.startsWith('blob:')) cleanUpBufferUrl(img.src);
+};
 
 const CustomImage = styled.figure.attrs({
-  draggable: false
+  draggable: false,
+  onLoad
 })``;
 
 export default CustomImage;
+
