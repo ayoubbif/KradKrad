ccf59198c31ae13ee6d9ef2ea9cee88e5e70129e
Ayoub <ayoub.biferdou@e-polytechnique.ma>
2023-01-18 04:00:48 +0100

    Fix resize handles losing focus

--- components/system/Window/RndWindow/index.tsx
+++ components/system/Window/RndWindow/index.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from 'react';
-import useRnd from './useRnd';
-import { Rnd } from 'react-rnd';
-import type { CSSProperties } from 'react';
+import useRnd from 'components/system/Window/RndWindow/useRnd';
 import { useProcesses } from 'contexts/process';
 import { useSession } from 'contexts/session';
+import type { CSSProperties } from 'react';
+import { useEffect, useRef } from 'react';
+import { Rnd } from 'react-rnd';
 import { DEFAULT_WINDOW_SIZE } from 'utils/constants';
 
 type RndWindowProps = {
@@ -12,20 +12,35 @@ type RndWindowProps = {
   style: CSSProperties;
 };
 
+const reRouteFocus =
+  (focusElement?: HTMLElement) =>
+  (element?: Element): void => {
+    element?.setAttribute('tabindex', '-1');
+    element?.addEventListener('mousedown', (event) => {
+      event.preventDefault();
+      focusElement?.focus();
+    });
+  };
+
 const RndWindow = ({ children, id, style }: RndWindowProps): JSX.Element => {
   const {
     processes: {
       [id]: { autoSizing, maximized }
     }
   } = useProcesses();
-
   const rndRef = useRef<Rnd | null>(null);
   const rndProps = useRnd(id, maximized);
   const { setWindowStates } = useSession();
 
   useEffect(() => {
-    const { current } = rndRef;
-    return () => {
+    const { current } = rndRef || {};
+    const [windowContainer, resizeHandleContainer] =
+      current?.resizableElement?.current?.children || [];
+    const resizeHandles = [...resizeHandleContainer?.children];
+
+    resizeHandles.forEach(reRouteFocus(windowContainer as HTMLElement));
+
+    return () =>
       setWindowStates((currentWindowStates) => ({
         ...currentWindowStates,
         [id]: {
@@ -33,7 +48,6 @@ const RndWindow = ({ children, id, style }: RndWindowProps): JSX.Element => {
           size: autoSizing ? DEFAULT_WINDOW_SIZE : current?.props?.size
         }
       }));
-    };
   }, [autoSizing, id, setWindowStates]);
 
   return (
@@ -44,3 +58,4 @@ const RndWindow = ({ children, id, style }: RndWindowProps): JSX.Element => {
 };
 
 export default RndWindow;
+
