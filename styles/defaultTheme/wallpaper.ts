1a3e791dbc73cb5e6771f588dde3a560212bb5c1
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-07-14 16:00:24 +0100

    Update wallpaper colors

--- styles/defaultTheme/wallpaper.ts
+++ styles/defaultTheme/wallpaper.ts
@@ -1,13 +1,13 @@
 import vantaClouds from 'components/system/Desktop/Wallpapers/vantaClouds';
 
 const wallpaper = vantaClouds({
-  skyColor: 0x2383c2,
-  cloudColor: 0x8aa497,
-  cloudShadowColor: 0x7660ae,
-  sunColor: 0xd1ca21,
+  skyColor: 0x2edfaa,
+  cloudColor: 0x8ab997,
+  cloudShadowColor: 0x766fed,
+  sunColor: 0xd78621,
   sunGlareColor: 0xed5031,
-  sunlightColor: 0xef7926,
-  speed: 1.5
+  sunlightColor: 0xee6b26,
+  speed: 1
 });
 
 export default wallpaper;
